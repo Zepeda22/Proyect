8b590fc1fb2bae299cd77c72f427982f46c32429
RZepeda22 <00175520@uca.edu.sv>
2021-11-15 21:23:37 -0600

    Add page login

--- my_social_network/src/App.jsx
+++ my_social_network/src/App.jsx
@@ -1,12 +1,21 @@
-
+import Login from "./pages/Login/Login";
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from "react-router-dom";
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>prueba</h1>
-      <h2>prueba</h2>
-    </div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>} exact />
+            
+          {/* <Route path="/" element={<p></p>} /> */}
+            
+        </Routes>
+    </Router>  
+    </div> 
   );
 }
 
