6dcc9a798330eb91fad76daa3d49e10d1c579c15
alexandrahm <00027117@uca.edu.sv>
2021-11-24 22:00:08 -0600

    uploading user services

---
+++ my_social_network/src/Services/User.services.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+const BASE_URL = "https://posts-pw2021.herokuapp.com/api/v1/post";
+
+export const useUserServices = {
+    getAll: async (parameter = {}, token) => {
+        const { limit, page} = parameter
+        const response = axios.get(`${BASE_URL}/all`, {
+            params:{
+                limit: limit,
+                page: page
+            },
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                console.log(response)
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+
+    getAllFavorites: async (token) => {
+        const response = axios.get(`${BASE_URL}/fav`, {
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+
+    getOne: async (id ,token) => {
+        const response = axios.get(`${BASE_URL}/one/${id}`, {
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+
+    patchLike: async (id ,token) => {
+        const response = axios.patch(`${BASE_URL}/like/${id}`, {
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+
+    patchFavorite: async (id ,token) => {
+        const response = axios.patch(`${BASE_URL}/fav/${id}`, {
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+
+    addComment: async (comment, id ,token) => {
+        const response = axios.patch(`${BASE_URL}/comment/${id}`, {
+            data:{
+                description: comment
+            },
+            headers: { 
+                Authorization: `Bearer ${token}` 
+            }
+        })
+            .then(response => {
+                return response.data;
+
+            })
+            .catch(error => {
+                console.log(error);
+                return {};
+            });
+        return response;
+    },
+};
