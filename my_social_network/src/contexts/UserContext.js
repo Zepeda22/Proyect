c28899cbd5dceadf7f873d271ce577a568908612
RZepeda22 <00175520@uca.edu.sv>
2021-11-25 20:04:26 -0600

    Changes in the UserContext

--- my_social_network/src/contexts/UserContext.js
+++ my_social_network/src/contexts/UserContext.js
@@ -1,78 +1,94 @@
-import React, { useState, useEffect, useMemo, useCallback } from "react";
-import userService from './../services/user.services';
+import {
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+    useMemo,
+    useCallback,
+} from "react";
+import { useServices } from "../Services/PW2021/Login.services";
 
-const UserContext = React.createContext();
 const TOKEN_KEY = "token";
+const UserContext = createContext();
 
 export const UserProvider = (props) => {
-    const [token, setToken] = useState(undefined);
     const [user, setUser] = useState(undefined);
-
+    const [token, setToken] = useState(undefined);
     useEffect(() => {
-        const verifyTokenAsync = async () => {
-            const lsToken = getToken();
-
-            if(lsToken) {
-                const { username, role } = await userService.verifyToken(lsToken);
-                if(username && role) {
-                    setUser({ username, role });
-                    setTokenAll(lsToken);
+        const verifyToken = async () => {
+            const localToken = getToken();
+            //console.log({ localToken });
+            if (localToken) {
+                const response = await useServices.verifyToken(localToken);
+                //console.log(response);
+                if (response.username && response.role) {
+                    setUser({ username: response.username, role: response.role });
+                    setToken(localToken);
                 }
             }
+            
+        };
+        console.log("Por el contex paso")
+        verifyToken();
+    }, [token]);
+
+    const getToken = () => {
+        return localStorage.getItem(TOKEN_KEY);
+    };
+
+    const setNewToken = (newToken) => {
+        if (newToken) {
+            localStorage.setItem(TOKEN_KEY, newToken);
+        } else {
+            localStorage.removeItem(TOKEN_KEY);
         }
 
-        verifyTokenAsync();
-    }, [token])
+        setToken(newToken);
+    };
 
-    const setTokenAll = (token) => {
-        localStorage.setItem(TOKEN_KEY, token);
-        setToken(token);
-    }
-
-    const login = useCallback((username, password)=> {
-        const loginAsync = async () => {
-            let status = false;
-            try {
-                const { token: tokenRes } = await userService.login(username, password);
-
-                if(tokenRes) {
-                    setTokenAll(tokenRes);
-                    status = true;
-                }
-            } catch (error) {
-                console.error(error);
-                console.error("Error in login");
-            } finally {
-                return status;
+    const login = useCallback((username, password) => {
+        const loginFetch = async (username, password) => {
+            const response = await useServices.login(username, password); // token y role
+            console.log(response) 
+                     if (response.token) {
+                setNewToken(response.token);
+                return true; // si se pudo
             }
+
+            return false;
         };
 
-        return loginAsync();
-    }, [])
+        return loginFetch(username, password);
+    }, []);
 
-    const logout = useCallback(() => {
+    const logout = () => {
+        setNewToken(undefined);
         setUser(undefined);
-        setTokenAll(undefined);
-    }, [])
+    }
 
-    const value = useMemo(()=> ({
-        token: token,
-        user: user,
-        login: login,
-        logout: logout
-    }), [token, user, login, logout]);
+    const providerValue = useMemo(() => {
+        return {
+            user: user,
+            login: login,
+            token: token,
+            logout: logout,
+            getToken: getToken,
+        };
+    }, [user, token]);
 
-    return <UserContext.Provider value={value} {...props} />;
-}
+    return (
+        <UserContext.Provider value={providerValue}>
+            {props.children}
+        </UserContext.Provider>
+    );
+};
 
 export const useUserContext = () => {
-    const context = React.useContext(UserContext);
+    const context = useContext(UserContext);
 
     if (!context) {
-        throw new Error("useUserContext() must be inside of UserProvider");
+        throw new Error("No estás dentro del UserProvider")
     }
 
     return context;
-}
-
-const getToken = () => localStorage.getItem(TOKEN_KEY);
+};
